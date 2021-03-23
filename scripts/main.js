14d8db12fe9b508b78e743828307d7c9c910a8d9
Michał Kucharski <michal.kucharski@creobit.pl>
2021-03-23 16:35:11 +0100

    Added view switching between weatherSearchView and weatherForecastView and fading animation

--- scripts/main.js
+++ scripts/main.js
@@ -26,8 +26,9 @@ const connectHTMLElems = () => {
 }
 
 const setupListeners = () => {
-    viewElems.searchInput.addEventListener('keydown', onEnterSubmit)
-    viewElems.searchButton.addEventListener('click', onClickSubmit)
+    viewElems.searchInput.addEventListener('keydown', onEnterSubmit);
+    viewElems.searchButton.addEventListener('click', onClickSubmit);
+    viewElems.returnToSearchBtn.addEventListener('click', returnToSearch);
 }
 
 const initializeApp = () => {
@@ -37,11 +38,51 @@ const initializeApp = () => {
 
 const onEnterSubmit = event => {
     if(event.key === 'Enter') {
+        fadeInOut();
         let query = viewElems.searchInput.value;
         getWeatherByCity(query)
-        .then(data => console.log(data));
+        .then(data => {
+            console.log(data)
+            fadeInOut();
+            switchView();
+        });
     }
 };
-const onClickSubmit = () => {};
+const onClickSubmit = () => {
+    fadeInOut();
+    let query = viewElems.searchInput.value;
+        getWeatherByCity(query)
+        .then(data => {
+            console.log(data)
+            fadeInOut();
+            switchView();            
+        });
+};
+
+const fadeInOut = () => {
+    if(viewElems.mainContainer.style.opacity === '1' || viewElems.mainContainer.style.opacity === '') {
+        viewElems.mainContainer.style.opacity = '0';
+    } else {
+        viewElems.mainContainer.style.opacity = '1';
+    }
+}
+
+const switchView = () => {
+    if(viewElems.weatherSearchView.style.display !== 'none') {
+        viewElems.weatherSearchView.style.display = 'none';
+        viewElems.weatherForecastView.style.display = 'block';
+    } else {
+        viewElems.weatherForecastView.style.display = 'none';
+        viewElems.weatherSearchView.style.display = 'flex';
+    }
+}
+
+const returnToSearch = () => {
+    fadeInOut();
+    setTimeout(()=> {
+        switchView();
+        fadeInOut();
+    }, 500);
+}
 
 document.addEventListener('DOMContentLoaded', initializeApp)
